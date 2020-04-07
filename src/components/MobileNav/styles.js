d68ac1e12ecef9a32f48c3a450b8f8e56df6b5a7
Rafael <ffael@live.com>
2020-04-07 11:33:41 -0400

    Refactored mobile nav styles

--- src/components/MobileNav/styles.js
+++ src/components/MobileNav/styles.js
@@ -4,12 +4,11 @@ export const Container = styled.section`
   display: flex;
   background: #fafafa;
   width: 100vw;
-  height: 500vh;
+  height: 100vh;
   position: absolute;
   z-index: 9999;
   top:0;
-  right: ${props => props.mobile ? 0 : "-150%" };
-  /* opacity: ${props => props.mobile ? 1: 0 }; */
-  /* display: ${props => props.disabled ? "": "none"}; */
+  right: 0;
+  display: ${props => props.disabled ? "none": ""};
   transition: all 0.5s ease-in;
 `
