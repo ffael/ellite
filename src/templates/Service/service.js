3fca9c070430150bf49a9e1e79a9f624125c34b2
Rafael <ffael@live.com>
2020-04-30 22:22:33 -0400

    Added Lightbox feature to gallery in services

--- src/templates/Service/service.js
+++ src/templates/Service/service.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { Link, graphql } from 'gatsby'
 
 import { FaArrowLeft } from 'react-icons/fa'
 import { Container, Content, CallForActionBtn } from './styles'
 import Gallery from 'react-photo-gallery'
+import Carousel, { Modal, ModalGateway} from 'react-images'
 
 import Layout from '../../layouts/pages'
 
@@ -35,24 +36,28 @@ export const query = graphql`
 `
 const ServicePage = (props)=>{
   const [currentImage, setCurrentImage] = useState(0)
-  const [viewerIsOpen, setViewerIsOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index)
+    setIsModalOpen(true)
+  }, [])
+
+  const handleClose = () =>{
+    setIsModalOpen(false)
+    setCurrentImage(0)
+  }
+
   const photos = []
+  const images = []
   if(props.data.service.gallery){
     props.data.service.gallery.map((item)=>{
       const { src, width, height } = item.fixed
       photos.push({src, width, height})
+      images.push({
+        source: src
+      })
     })
-  }
-  
-  const openLightBox = useCallback((event, { photo, index })=>{
-    setCurrentImage(index)
-    setViewerIsOpen(true)
-    console.log(photo)
-  }, [])
-
-  const closeLightBox = () =>{
-    setCurrentImage(0)
-    setViewerIsOpen(false)
   }
   
   return(
@@ -72,7 +77,16 @@ const ServicePage = (props)=>{
             <p>{props.data.service.description.description}</p>
           </section>
         </Content>
-        <Gallery photos={photos} direction={"column"}/>
+        <Gallery photos={photos} direction={"column"} onClick={openLightbox}/>
+        
+          
+        <ModalGateway>
+          {isModalOpen ? 
+            <Modal onClose={()=> handleClose()} closeOnEsc={true}>
+              <Carousel currentIndex={currentImage} views={photos} />
+            </Modal>
+            : null }
+        </ModalGateway>
         {/* { props.data.service.gallery !== null ? 
           <>
             
