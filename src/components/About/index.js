ec191ff2114224387d844dcd19ca350d392c76c9
Rafael <ffael@live.com>
2020-05-02 21:26:07 -0400

    Added responsive institutional video on About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Content, Card, CallForActionBtn } from './styles'
 import { useStaticQuery, graphql, Link } from 'gatsby'
+import ResponsiveEmbed from 'react-responsive-embed'
 
 const About = ({mainPage = true}) =>{
   const data = useStaticQuery(graphql`
@@ -28,6 +29,9 @@ const About = ({mainPage = true}) =>{
         </Card>
         <div>
           <p>{data.contentfulAbout.description.description}</p>
+          { mainPage === false ?
+            <ResponsiveEmbed src='https://www.youtube.com/embed/9mQHkZe52uk' allowfullscreen />
+          : ""}
           { mainPage ?
             <CallForActionBtn>
               <Link className="box-shadow" to='/about'>Get to Know Us</Link>
